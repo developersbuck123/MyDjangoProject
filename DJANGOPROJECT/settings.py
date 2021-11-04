2a4531c4c65b7e3fabed32d2837f61f74610dda6
developersbuck123 <93202297+developersbuck123@users.noreply.github.com>
2021-11-04 16:42:19 +0530

    Add files via upload

--- DJANGOPROJECT/settings.py
+++ DJANGOPROJECT/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['my-django-project-mebmv.ondigitalocean.app', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -71,17 +70,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DJANGOPROJECT.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+...
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'StudentDB',
+        'CLIENT': {
+            'host': 'localhost:27017'
+        }
     }
 }
-
+...
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -114,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
